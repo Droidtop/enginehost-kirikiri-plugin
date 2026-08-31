d7d9197db9c45e4632293352a4df2d14e7bb9a79
bi0shacker001 <bioshacker001@gmail.com>
2026-08-31 14:00:28 -0400

    Make KiriKiri compatibility version agnostic

--- project/android/app/java/org/tvp/kirikiri2/KR2Activity.java
+++ project/android/app/java/org/tvp/kirikiri2/KR2Activity.java
@@ -306,12 +306,8 @@ public class KR2Activity extends Cocos2dxActivity implements ActivityCompat.OnRe
 		String engineHostPath = getIntent().getStringExtra("dev.enginehost.runtime.PATH");
 		if (engineHostPath != null) {
 			String context = getIntent().getStringExtra("dev.enginehost.runtime.ENGINE_CONTEXT");
-			String version = getIntent().getStringExtra("dev.enginehost.runtime.ENGINE_VERSION");
 			if (context != null && !context.isEmpty() && !"default".equals(context)) {
 				throw new IllegalArgumentException("Unsupported KiriKiri engineContext: " + context);
-			}
-			if (!"2.31.2009.825".equals(version) && !"2.32".equals(version) && !"2.32.0".equals(version)) {
-				throw new IllegalArgumentException("Unsupported KiriKiri engineVersion: " + version);
 			}
 			if (!new File(engineHostPath).isDirectory()) {
 				throw new IllegalArgumentException("enginehost did not provide a valid KiriKiri game folder");
